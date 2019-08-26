07a4af42992457c00dc393ebbf417ec9a3523384
sierra mills <alex49er13@gmail.com>
2019-08-26 14:52:03 +0000

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
-    def after_sign_in_path_for(word)
-        company_path
-    end
+    # def after_sign_in_path_for(word)
+    #     company_path
+    # end
 
 end
